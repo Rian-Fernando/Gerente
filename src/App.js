847c8978897610664f3fdb46b2eb71d689cd6ee3
Rian Fernando <rian.fernando2006@gmail.com>
2025-04-01 12:48:08 -0400

    Updated App.js and added TaskList component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TaskList from './components/TaskList';  // Intentional issue: TaskList.js is empty
+import './styles.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Gerente - Task Manager</h1>
+            <TaskList />  {/* This will cause an error initially */}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/TaskList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TaskList() {
+    return (
+        <div>
+            <h2>Your Tasks</h2>
+            <ul>
+                {/* Tasks will be displayed here */}
+            </ul>
+        </div>
+    );
+}
+
+export default TaskList;
