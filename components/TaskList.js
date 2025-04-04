36e9becd5c0cdf4467e80eed090757e4ae1e2a20
Rian Fernando <rian.fernando2006@gmail.com>
2025-04-04 10:20:54 -0400

    TaskList now dynamically displays tasks

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,3 +1,13 @@
 import React from 'react';
-const TaskList = () => <div>Task List Component</div>;
+
+const TaskList = ({ tasks }) => {
+  return (
+    <ul>
+      {tasks.map((task, index) => (
+        <li key={index}>{task}</li>
+      ))}
+    </ul>
+  );
+};
+
 export default TaskList;
